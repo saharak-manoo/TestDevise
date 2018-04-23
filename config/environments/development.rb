e5e3eba1bc19868797e0d986236f83650ad28f0a
SaharakBanana <mju5804101398@gmaejo.mju.ac.th>
2018-04-23 11:19:34 +0700

    first commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,13 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
+  config.action_mailer.default_options = {from:'no-reply@example.com'}
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :admins
   get 'home/index'
 	devise_for :users
 	authenticated :user do

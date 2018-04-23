dd9473edc2a144b2d711f8070aa33252d1d3fde1
SaharakBanana <mju5804101398@gmaejo.mju.ac.th>
2018-04-23 10:07:10 +0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'home/index'
+	devise_for :users
+	authenticated :user do
+		root to: 'home#index'
+	end
+	unauthenticated :user do
+		devise_scope :user do
+			root to: 'devise/sessions#new'
+		end
+	end
+
+end
